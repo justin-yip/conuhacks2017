ad00b0773d0ad288b31b1f04c8cad436c9884394
Helgi Tomas Gislason <helgso@gmail.com>
2017-01-21 12:43:42 -0500

    NLP now processes given string and returns a Sentece object with lots of metadata

--- routes/nlp.js
+++ routes/nlp.js
@@ -1,10 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+// Natural Language Processing
+let nlp = require('nlp_compromise');
+
+router.get('/', function(req, res, next) {
+	var response = "To use NLP, "
+	+"you could do: "
+	+"localhost:3000/nlp/I want some Nike shoes";
+	res.send(response)
+});
+
 /* GET routes. */
 router.get('/:twitterstatus', function(req, res, next) {
+	// Retrieve GET parameter
 	var twitterStatus = req.params.twitterstatus;
-	res.send(twitterStatus);
+
+	// Pass it to NLP compromise for processing
+	var sentence = nlp.sentence(twitterStatus);
+
+	var jsonResult = JSON.stringify(sentence);
+
+	// Return result for debugging
+	res.send(jsonResult);
 });
 
 module.exports = router; 

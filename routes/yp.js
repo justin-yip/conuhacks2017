3d3e7659db0822b8b2838698b48aef5387a8a0e4
Justin Yip <justin.n.yip@gmail.com>
2017-01-21 19:35:52 -0500

    hackathon api for yp

--- routes/yp.js
+++ routes/yp.js
@@ -4,33 +4,47 @@ var request = require('request');
 
 router.post('/find', function(req, res, next) {
     let data = {
-        what: req.body.what,
-        where : req.body.where,
-        pgLen: 5,
-        pg: 1,
-        dist: 5,
-        fmt: "JSON",
+        latitude: req.body.latitude,
+        longitude: req.body.longitude,
+        radius: 5,
+        show: "deal",
+        keyword: (req.body.keyword).join("+"),
         lang: "en",
-        UID: "1",
-        apikey: "uvu5duaqz94wexb8sqghqm4q"
-    };
+    }
+    // let data = {
+    //     what: req.body.what,
+    //     where : req.body.where,
+    //     pgLen: 5,
+    //     pg: 1,
+    //     dist: 5,
+    //     fmt: "JSON",
+    //     lang: "en",
+    //     UID: "1",
+    //     apikey: "uvu5duaqz94wexb8sqghqm4q"
+    // };
 
     findBusiness(data, res);
 });
 
 function findBusiness(data, response){
-    let url = "http://api.sandbox.yellowapi.com/FindBusiness/?";
+    let url = "http://dcr.yp.ca/api/search/popular?";
     url += encodeQueryData(data)
     
     request.get(url, function(err, res, body) {
         if (err) {
             console.log(err)
             response.status(500).send(err);
-        }else if(res.status === "200"){
-            console.log(body)
-            response.send(body)
         }
-        response.status(500).send("status not 200")
+        var body = JSON.parse(body);
+        if(body.data && body.data.length > 0){
+            let deal = body.data[0].result.Translation.en;
+            let merchant = body.data[0].result.Merchants[0].Translation.en;
+            let str = merchant.name +"\nDeal: "+deal.title;
+            response.send(str)
+        }else{
+
+        }
+
     });
 }
 

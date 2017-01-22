35cfad993cecda436e408aea16dab84a506c80f3
Rakavy <rakavy_12@hotmail.com>
2017-01-22 01:36:21 -0500

    web page for more results

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var id = req.query.id;
+  var long = req.query.long;
+  var lat = req.query.lat;
+  var address = req.query.address;
+  var keyword = req.query.kw;
+
+  var arr = [{id:"2",dealUrl:"http://www.youtube.com",distance:"1232 km",dealName:"Deal 1"},{name:"Hello12",id:"2",url:"http://www.google.ca",dealUrl:"http://www.youtube.com",distance:"1232 km",dealName:"Deal 1"}]; 
+
+  console.log(id);
+  console.log(long)
+  console.log(lat)
+  console.log(address)
+  console.log(keyword)
+  res.render('index', {array: arr});
 });
 
 module.exports = router;
